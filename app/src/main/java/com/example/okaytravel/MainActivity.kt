83fe05b671ea07ed75049a385d4662ed8284174c
makeitokay <andrey.mu11@yandex.ru>
2020-02-01 22:50:24 +0500

    save input data

--- app/src/main/java/com/example/okaytravel/MainActivity.kt
+++ app/src/main/java/com/example/okaytravel/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.okaytravel
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setLabelText()
+    }
+
+    fun saveText(view: View) {
+        val sharedPref = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+        with (sharedPref.edit()) {
+            putString("textData", textData.text.toString())
+            commit()
+        }
+        setLabelText()
+    }
+
+    fun setLabelText() {
+        val sharedPref = getSharedPreferences("sharedPref", Context.MODE_PRIVATE) ?: return
+        val savedText = sharedPref.getString("textData", "")
+        savedTextView.text = savedText
     }
 }
