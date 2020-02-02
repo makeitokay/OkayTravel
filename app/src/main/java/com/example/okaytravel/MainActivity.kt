8272d7039be3dac15fd5897c52a283593cf567ae
makeitokay <andrey.mu11@yandex.ru>
2020-02-02 21:06:20 +0500

    Создал LoginActivity

--- app/src/main/java/com/example/okaytravel/MainActivity.kt
+++ app/src/main/java/com/example/okaytravel/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var emailData: String? = intent.getStringExtra("email")
+        var password: String? = intent.getStringExtra("password")
+
+        toast(emailData, password)
         setLabelText()
     }
 
@@ -27,5 +34,13 @@ class MainActivity : AppCompatActivity() {
         val sharedPref = getSharedPreferences("sharedPref", Context.MODE_PRIVATE) ?: return
         val savedText = sharedPref.getString("textData", "")
         savedTextView.text = savedText
+    }
+
+    fun toast(email: String?, password: String?) {
+        Toast.makeText(
+            this,
+            "Nice to meet you, ${email ?: return}. Nice password ${password ?: return}",
+            Toast.LENGTH_LONG)
+            .show()
     }
 }
