27038d631e0ada7d7838bf3f2551614f84256521
makeitokay <andrey.mu11@yandex.ru>
2020-02-19 12:06:29 +0500

    Создал карту в TripsMapFragment

--- app/src/main/java/com/example/okaytravel/activities/fragments/TripsMapFragment.kt
+++ app/src/main/java/com/example/okaytravel/activities/fragments/TripsMapFragment.kt
@@ -7,14 +7,46 @@ import android.view.ViewGroup
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.example.okaytravel.R
 import com.example.okaytravel.views.TripsMapView
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import kotlinx.android.synthetic.main.fragment_trips_map.*
 
 class TripsMapFragment: MvpAppCompatFragment(), TripsMapView {
+
+    private val startPoint = Point(53.413925, 58.994046)
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        MapKitFactory.setApiKey(getString(R.string.mapkitAccessToken))
+        MapKitFactory.initialize(this.requireActivity())
+
         return inflater.inflate(R.layout.fragment_trips_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        mapView.map.move(
+            CameraPosition(startPoint, 14.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 5f),
+            null)
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        MapKitFactory.getInstance().onStop()
+        mapView.onStop()
     }
 
     override fun showMessage(message: String) {
